5ca0e0b9407bad3c6a6f6c40094df87eddb381e5
Colin Perry <123716435+colinperry07@users.noreply.github.com>
2026-01-06 00:53:55 -0500

    Replace individual stats with a stats dictionary
    Refactor stats management and random point distribution

--- scripts/Fyte.gd
+++ scripts/Fyte.gd
@@ -4,55 +4,45 @@ extends KinematicBody2D
 var catch_rate = 100
 const MAX_CATCH_RATE = 100
 
-var attack = 0
-var defense = 0
-var stamina = 0 # the higher this the higher hp
+var stats = { # stat name -> id
+	"attack": 0,
+	"defense": 0,
+	"stamina": 0
+}
+
+const MAX_STAT_VALUE = 15
+
 var iv # individual value, calculates the fufillment of the random stats
 
-var type # the type of fyte it is, whether pyro or geo, etc...
+enum FyteType { PYRO, GEO, ELEKTRO, HYDRO, PETRA}
+
+var type # the type of fyte it is of type FyteType
 var moveset = [] # this will be based on the type
 var energy_produced # also based on the type
 
 
-# 0 star < 50
-# 1 star >= 50
-# 2 star >= 75
-# 3 star >= 90
-# 4 star == 100
+enum FyteRating {
+	ZERO = 0,
+	ONE = 50,
+	TWO = 65,
+	THREE = 80,
+	FOUR = 100
+}
 
 func _ready():
 	randomize() # initializes random seed
 	
-	# distributes points across stats
-	var unallocated = 45
-	print(unallocated)
-	for i in range(unallocated):
-		var rng = randi()%3
-		match rng:
-			0:
-				if attack >= 15 and defense >= 15:
-					stamina += 1
-				elif attack >= 15 and stamina >= 15:
-					defense += 1
-				else:
-					attack += 1
-			1:
-				if defense >= 15 and attack >= 15:
-					stamina += 1
-				elif defense >= 15 and stamina >= 15:
-					attack += 1
-				else:
-					defense += 1
-			2:
-				if stamina >= 15 and attack >= 15:
-					defense += 1
-				elif stamina >= 15 and defense >= 15:
-					attack += 1
-				else:
-					stamina += 1
+	# distributes stats randomly
+	var unallocated_points = randi() % 46 # picks a random amount of points between none and max stat points
+	var keys = stats.keys()
+	while unallocated_points > 0:
+		var key = keys[randi() % len(keys)]
+		if stats[key]< MAX_STAT_VALUE:
+			stats[key] += 1
+			unallocated_points -= 1
 	
-	print(attack, ' ', defense,' ', stamina)
+	print(stats)
 
 
-func calculate_catch_rate(stats):
+func calculate_catch_rate():
 	pass
