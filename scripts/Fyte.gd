48b5ebf7f9dc91ad5569b41d76f4e1a2ab469af9
Colin Perry <123716435+colinperry07@users.noreply.github.com>
2026-01-06 01:51:46 -0500

    Enhance Fyte class with new stats and enums
    Refactor Fyte class to include catch rate and rating calculations.

--- scripts/Fyte.gd
+++ scripts/Fyte.gd
@@ -1,8 +1,7 @@
 extends KinematicBody2D
 
 
-var catch_rate = 100
-const MAX_CATCH_RATE = 100
+const MAX_STAT_VALUE = 15
 
 var stats = { # stat name -> id
 	"attack": 0,
@@ -10,16 +9,18 @@ var stats = { # stat name -> id
 	"stamina": 0
 }
 
-const MAX_STAT_VALUE = 15
-
+var hp # not sure how this is going to be calculated or come into play yet. but will be based heavily on stamina
 var iv # individual value, calculates the fufillment of the random stats
-var rating # this is the star rating of the Fyte
 
-enum FyteType { PYRO, GEO, ELEKTRO, HYDRO, PETRA }
+var rating # this is the star rating of the Fyte
+var catch_rate # this is the likelihood of catching the Fyte
 
 var type # the type of fyte it is of type FyteType
 var moveset = [] # this will be based on the type
 var energy_produced # also based on the type
+
+# enums for calculating different values
+enum FyteType { PYRO, GEO, ELEKTRO, HYDRO, PETRA }
 
 enum FyteRating { # the rating of the Fyte based on it's IV
 	ZERO = 50,
@@ -28,6 +29,14 @@ enum FyteRating { # the rating of the Fyte based on it's IV
 	THREE = 100,
 	FOUR = 101
 } 
+
+enum CatchDifficulty {
+	GUARENTEED = 10,
+	EASY = 25,
+	MEDIUM = 50,
+	HARD = 75,
+	IMPOSSIBLE = 100
+}
 
 
 func _ready():
@@ -43,20 +52,27 @@ func _ready():
 			unallocated_points -= 1
 	
 	calculate_individual_value()
+	calculate_catch_rate()
 
 
 func calculate_individual_value():
+	# calculates the total IV (Individual Value) of this Fyte
 	var keys = stats.keys()
 	var total = 0
 	for key in keys:
 		total += stats[key]
 	iv = total/.45
 	
+	# iterates through the FyteRating enum values and finds the star rating for this specific Fyte
 	for value in FyteRating.values():
 		if iv < value:
 			rating = FyteRating.find_key(value)
 			break
 
-func calculate_catch_rate():
-	pass
-	
+func calculate_catch_rate(): # calculates catch rate based on the Fyte's stamina stat value
+	var difficulty = stats['stamina']/.15
+	for value in CatchDifficulty.values():
+		if difficulty < value:
+			catch_rate = CatchDifficulty.find_key(value)
+			break
+
