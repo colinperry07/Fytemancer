475d965fbc937346f64e8c7e12a8b5f109bd6756
Colin Perry <123716435+colinperry07@users.noreply.github.com>
2026-01-06 22:49:32 -0500

    Refactor gravity and fall speed constants in Player.gd

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -4,19 +4,27 @@ extends KinematicBody2D
 export var speed = 676
 export var accel = 0.125
 export var jump = 1000
-export var GRAVITY = 75
-export var MAX_FALL_SPEED = 3333
+const GRAVITY = 75
+const MAX_FALL_SPEED = 1200
 var velocity = Vector2.ZERO
 
-func _physics_process(delta):
+# node variables
+onready var fyte_siphon = $FyteSiphon
+
+
+func _physics_process(_delta):
 	
-	# movement logic
+	movement_logic()
+
+
+func movement_logic():
+	
 	var dir = 0
 	if Input.is_action_pressed("LEFT"):
 		dir -= 1
 	if Input.is_action_pressed("RIGHT"):
 		dir += 1
-	
+
 	if dir != 0:
 		velocity.x = lerp(velocity.x, speed*dir, accel)
 	else:
@@ -27,11 +35,11 @@ func _physics_process(delta):
 		if Input.is_action_pressed("JUMP"):
 			velocity.y -= jump
 	else:
-		if velocity.y == MAX_FALL_SPEED:
+		if velocity.y >= MAX_FALL_SPEED:
 			velocity.y = MAX_FALL_SPEED
 		else:
 			velocity.y += GRAVITY
 	
 	velocity = move_and_slide(velocity, Vector2.UP)
-	
-	# next logc block here
+
+
