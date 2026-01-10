1d17ee69cd8e797a9ecd81f3f4577b8bfa3c16ed
Colin Perry <123716435+colinperry07@users.noreply.github.com>
2026-01-10 00:28:59 -0500

    Rename loop variable in spawn function
    Updated loop variable name for clarity.

--- scripts/Sandbox.gd
+++ scripts/Sandbox.gd
@@ -6,7 +6,7 @@ var fyte_spawn_num = 2
 
 func _ready():
 	randomize()
-	for i in range(fyte_spawn_num):
+	for _i in range(fyte_spawn_num): # temporary spawn algo
 		var fyte_instance = fyte_scene.instance()
 		fyte_instance.position = Vector2(randi()%1000, 100)
 		add_child(fyte_instance)
