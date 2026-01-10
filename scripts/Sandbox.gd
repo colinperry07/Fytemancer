b5e284e05159b6f0447ea8761da4c65122a0cea1
Colin Perry <123716435+colinperry07@users.noreply.github.com>
2026-01-10 00:17:40 -0500

    Add files via upload

---
+++ scripts/Sandbox.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+onready var fyte_scene = preload("res://scenes/Fyte.tscn")
+
+var fyte_spawn_num = 2
+
+func _ready():
+	randomize()
+	for i in range(fyte_spawn_num):
+		var fyte_instance = fyte_scene.instance()
+		fyte_instance.position = Vector2(randi()%1000, 100)
+		add_child(fyte_instance)
